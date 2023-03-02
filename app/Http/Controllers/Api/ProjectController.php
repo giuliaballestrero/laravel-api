1fd46a9f8e7caf050c65a398be9cac26be7ab942
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-02 15:47:30 +0100

    Add api show route and test api json results

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,13 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
     public function index () {
+
+        //prendo tutti i post
+        $projects = Project::paginate(15);
+
+        //la risposta mi restituisce tutti i projects in formato json
         return response()->json([
-            'name' => "Juls",
-            'text' => "prova"
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show (Project $project) { //uso la dependency injection
+
+        //la risposta mi restituisce il project da visualizzare in formato json
+        return response()->json([
+            'success' => true,
+            'results' => $project
         ]);
     }
 }
